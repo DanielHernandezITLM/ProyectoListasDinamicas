232331da4aa075ee8626eb7d05c929a6d7db096c
DanielhernandezITLM <danihm95@gmail.com>
2018-03-15 14:22:16 -0700

    agregar data

--- AndroidStudioProjects/proyectoListasDinamicas/app/src/main/java/com/example/itlm/proyectolistasdinamicas/Form.java
+++ AndroidStudioProjects/proyectoListasDinamicas/app/src/main/java/com/example/itlm/proyectolistasdinamicas/Form.java
@@ -1,29 +1,55 @@
 package com.example.itlm.proyectolistasdinamicas;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Form extends AppCompatActivity {
+    EditText nombre, telefono, correo;
+    String nombreMandar,telefonoMandar, correoMandar, todo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        nombre = (EditText) findViewById(R.id.editText);
+        telefono = (EditText) findViewById(R.id.editText2);
+        correo = (EditText) findViewById(R.id.editText3);
+
+
+
     }
 
+
+    public void cancel(View view) {
+
+        Intent i = new Intent(Form.this, MainActivity.class);
+        startActivity(i);
+
+    }
+
+    public void addContact(View view) {
+        nombreMandar = String.valueOf(nombre.getText());
+        telefonoMandar = String.valueOf(telefono.getText());
+        correoMandar = String.valueOf(correo.getText());
+
+        Intent addF = getIntent();
+        addF.putExtra("data1",nombreMandar);
+        addF.putExtra("data2",telefonoMandar);
+        addF.putExtra("data3",correoMandar);
+        setResult(RESULT_OK,addF);
+        finish();
+
+    }
 }
--- AndroidStudioProjects/proyectoListasDinamicas/app/src/main/java/com/example/itlm/proyectolistasdinamicas/MainActivity.java
+++ AndroidStudioProjects/proyectoListasDinamicas/app/src/main/java/com/example/itlm/proyectolistasdinamicas/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.itlm.proyectolistasdinamicas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<String> ListaNombres;
     ListView listView;
     int tamañolista;
+    String first, second, third;
 
     //TextView tvNumeroRegistros;
     //String datosUsuario = "";
@@ -28,44 +37,70 @@ public class MainActivity extends AppCompatActivity {
         ListaNombres = new ArrayList<String>();
         listView = (ListView) findViewById(R.id.listView);
 
-        llenaDatos(ListaContactos);
+       // llenaDatos(ListaContactos);
         tamañolista = ListaContactos.size();
 
-        llenarListaNombre();
 
+        //llenarListaNombre();
+
+
+
+        /*listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+        });*/
+
+    }
+
+
+
+    /*void llenaDatos(ArrayList listaContactos){
+
+        ListaContactos.add(new Contacto(first,second,third));
+
+    }*/
+
+    void muestraDatos(){
         ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,ListaNombres);
 
-        listView.setAdapter(adaptador);
-        /*tvNumeroRegistros = (TextView)findViewById(R.id.tvNumeroRegistros);
-
-        for(int i=0;i < tamañolista;i++){
-            datosUsuario = datosUsuario + " " + ListaContactos.get(i).getNombre() + " " + ListaContactos.get(i).getTelefono() + " " + ListaContactos.get(i).geteMail() + "\n";
-
-        }
-        tvNumeroRegistros.setText(datosUsuario);
-        */
-
-
-    }
-
-    void llenaDatos(ArrayList listaContactos){
-        ListaContactos.add(new Contacto("Daniel","123123123","danihm95@gmail.com"));
-        ListaContactos.add(new Contacto("Oscar","21342134","oscar@gmail.com"));
-        ListaContactos.add(new Contacto("Juana","567234236","juana@gmail.com"));
-        ListaContactos.add(new Contacto("Hugo","0129831234","hugo@gmail.com"));
-        ListaContactos.add(new Contacto("Daniel","123123123","danihm95@gmail.com"));
-        ListaContactos.add(new Contacto("Oscar","21342134","oscar@gmail.com"));
-        ListaContactos.add(new Contacto("Juana","567234236","juana@gmail.com"));
-        ListaContactos.add(new Contacto("Hugo","0129831234","hugo@gmail.com"));
-        ListaContactos.add(new Contacto("Daniel","123123123","danihm95@gmail.com"));
-        ListaContactos.add(new Contacto("Oscar","21342134","oscar@gmail.com"));
-        ListaContactos.add(new Contacto("Juana","567234236","juana@gmail.com"));
-        ListaContactos.add(new Contacto("Hugo","0129831234","hugo@gmail.com"));
-    }
-
-    void llenarListaNombre(){
         for(int i=0;i<tamañolista;i++){
             ListaNombres.add(ListaContactos.get(i).getNombre());
         }
+
+        listView.setAdapter(adaptador);
     }
+
+    /*void llenarListaNombre(){
+        for(int i=0;i<tamañolista;i++){
+            ListaNombres.add(ListaContactos.get(i).getNombre());
+        }
+    }*/
+
+    public void add(View view) {
+
+        Intent i = new Intent(MainActivity.this, Form.class);
+        startActivityForResult(i, 1);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==1){
+            first = data.getExtras().getString("data1");
+            second = data.getExtras().getString("data2");
+            third = data.getExtras().getString("data3");
+
+            Toast.makeText(this, first +" añadido", Toast.LENGTH_SHORT).show();
+            ListaContactos.add(new Contacto(first,second,third));
+            muestraDatos();
+
+
+        }
+
+    }
+
 }
